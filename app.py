2ff71a3b1262106a30f009bd31e01853f0820b04
yeseuljoyceeom <69527769+yeseuljoyceeom@users.noreply.github.com>
2021-03-08 02:35:13 +0900

    login,signup,logout

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
-from flask import Flask, render_template, jsonify, request
+import jwt
+import hashlib
+from functools import wraps
+from datetime import datetime, timedelta
+from flask import Flask, render_template, jsonify, request, redirect, url_for, g, make_response, flash
 
 app = Flask(__name__)
 
@@ -7,8 +11,50 @@ from pymongo import MongoClient
 client = MongoClient('localhost', 27017)
 db = client.dbreflix
 
+# jwt secret key
+SECRET_KEY = 'cherry tomato'
+COOKIE_KEY = 'token_give'
 
-## HTML 화면 보여주기
+# secret_key for flash()
+app.secret_key = 'flash_secret'
+
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        # 쿠키에서 token_give 가져오기
+        token_receive = request.cookies.get(COOKIE_KEY)
+        print('token_receive :', token_receive)
+
+        if token_receive is None:
+            # token이 없는 경우
+            flash('로그인 후 이용해 주세요')
+            return redirect(url_for('login'))
+
+        try:
+            # 전달받은 token이 위조되었는지 확인 (단방향이기 때문에 비밀번호와 마찬가지로 해쉬처리하여 동일한지 비교)
+            # SECRET_KEY를 모르면 동일한 해쉬를 만들 수 없음
+            payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+
+        except jwt.InvalidTokenError:
+            # 토큰 없거나 만료되었거나 올바르지 않은 경우 로그인 페이지로 이동
+            flash('로그인 후 이용해 주세요')
+            return redirect(url_for('login'))
+
+        # g는 각각의 request 내에서만 값이 유효한 스레드 로컬 변수입니다.
+        # 사용자의 요청이 동시에 들어오더라도 각각의 request 내에서만 g 객체가 유효하기 때문에 사용자 ID를 저장해도 문제가 없습니다.
+        g.user = db.user.find_one({'userId': payload["id"]})
+        print(g.user['nickname'])
+
+        # 로그인 성공시 다음 함수 실행
+        return f(*args, **kwargs)
+
+    return decorated_function
+
+
+#################################
+# HTML 응답 API
+#################################
 @app.route('/')
 def mainpage():
     return render_template('index.html')
@@ -40,10 +86,26 @@ def board():
 
 
 @app.route('/boardpost')
+@login_required
 def boardpost():
     return render_template('boardpost.html')
 
 
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+#################################
+# JSON 응답 API
+#################################
+
+# 영화검색페이지 영화리스트
 @app.route('/contents', methods=["GET"])
 def printcontents():
     page = int(request.args.get('page', 1))
@@ -202,6 +264,7 @@ def printcontents():
     return jsonify({'result': 'success', 'contents': pageContents, 'total': totalContents})
 
 
+# 영화 개별페이지
 @app.route('/content', methods=["GET"])
 def printcontent():
     id = int(request.args.get('id'))
@@ -210,29 +273,35 @@ def printcontent():
     return jsonify({'result': 'success', 'content': content})
 
 
+# 영화 개별페이지 댓글 저장
 @app.route('/movie_comment', methods=["POST"])
+@login_required
 def post_movie_comment():
     contentId = int(request.form["id_give"])
     star = int(request.form["star_give"])
     comment = request.form["comment_give"]
     date = request.form["date_give"]
 
+    user = g.user['nickname']
+
     db.totContents.update_one({"contentId": contentId},
-                              {"$push": {"comment": {"star": star, "text": comment, "date": date}}})
+                              {"$push": {"comment": {"user": user, "star": star, "text": comment, "date": date}}})
     return jsonify({'result': 'success', 'msg': '리뷰 작성 완료!'})
 
 
-@app.route('/movie_comment', methods=["GET"])
-def read_comment():
-    contentId = int(request.args.get("id_give"))
-    try:
-        comments = db.totContents.find_one({"contentId": contentId})["comment"]
-    except KeyError:
-        comments = "None"
+# 영화 개별페이지 댓글 보여주기
+# @app.route('/movie_comment', methods=["GET"])
+# def read_comment():
+#     contentId = int(request.args.get("id_give"))
+#     try:
+#         comments = db.totContents.find_one({"contentId": contentId})["comment"]
+#     except KeyError:
+#         comments = "None"
+#
+#     return jsonify({'result': 'success', 'comments': comments})
 
-    return jsonify({'result': 'success', 'comments': comments})
 
-
+# 자유게시판 글리스트
 @app.route('/posts', methods=["GET"])
 def read_posts():
     page = int(request.args.get('page_give'))
@@ -244,21 +313,29 @@ def read_posts():
 
 
 @app.route('/posts', methods=["POST"])
+@login_required
 def post_post():
     postId = int(list(db.board.find({}, {"_id": False}))[-1]['postId']) + 1
     title = request.form['title_give']
     content = request.form['content_give']
     date = request.form['date_give']
+
+    user = g.user['nickname']
+    userId = g.user['userId']
+
     doc = {
         'postId': postId,
         'title': title,
         'content': content,
-        'date': date
+        'date': date,
+        'user': user,
+        'userId':userId
     }
     db.board.insert_one(doc)
     return jsonify({'result': 'success', 'msg': '글 작성 완료!'})
 
 
+# 자유게시판 글 개별페이지
 @app.route('/post', methods=["GET"])
 def read_post():
     postId = int(request.args.get('id_give'))
@@ -267,14 +344,95 @@ def read_post():
 
 
 @app.route('/board_comment', methods=["POST"])
+@login_required
 def post_board_comment():
     postId = int(request.form['id_give'])
     comment = request.form['comment_give']
     date = request.form['date_give']
 
+    user = g.user['nickname']
+
     db.board.update_one({"postId": postId},
-                        {"$push": {"comment": {"comment": comment, "date": date}}})
+                        {"$push": {"comment": {'user': user, "comment": comment, "date": date}}})
     return jsonify({'result': 'success', 'msg': '댓글 작성 완료!'})
+
+@app.route('/delete_post', methods=["POST"])
+def delete_post():
+    postId_receive = int(request.form['postId_give'])
+    db.board.delete_one({'postId':postId_receive})
+
+    return jsonify({'result': 'success', 'msg': '삭제 완료'})
+
+
+#로그인, 회원가입, 로그아웃
+@app.route('/confirmId', methods=["GET"])
+def confirmId():
+    id = request.args.get('id_give')
+    user = list(db.user.find({'userId': id}, {'_id': False}))
+    if len(user) != 0:
+        return jsonify({'result': 'fail', 'msg': '중복된 아이디입니다.'})
+
+    return jsonify({'result': 'success', 'msg': '사용 가능한 아이디입니다.'})
+
+
+@app.route('/signup', methods=["POST"])
+def signup():
+    id_receive = request.form['id_give']
+    pw_receive = request.form['pw_give']
+    nickname_receive = request.form['nickname_give']
+
+    # pw를 sha256 방법(단방향)으로 암호화
+    pw_hash = hashlib.sha256(pw_receive.encode('utf-8')).hexdigest()
+
+    db.user.insert_one({'userId': id_receive, 'userPw': pw_hash, 'nickname': nickname_receive})
+
+    return jsonify({'result': 'success', 'msg': '🎉 회원가입을 완료했습니다.🎉'})
+
+
+@app.route('/login_api', methods=['POST'])
+def login_api():
+    id_receive = request.form['id_give']
+    pw_receive = request.form['pw_give']
+
+    # pw를 sha256 방법(단방향)으로 암호화
+    pw_hash = hashlib.sha256(pw_receive.encode('utf-8')).hexdigest()
+
+    # id, 암호화된 pw을 가지고 해당 유저를 찾기
+    user = db.user.find_one({'userId': id_receive, 'userPw': pw_hash})
+
+    if user is not None:
+        # jwt 토큰 발급
+        payload = {
+            'id': user['userId'],  # user id
+            'exp': datetime.utcnow() + timedelta(minutes=60)  # 만료 시간
+        }
+        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
+        print(f'token : {token}')
+        res = make_response(jsonify({'result': 'success', 'msg': f'{user["nickname"]}님 안녕하세요 🙇🏻‍♂️'}))
+
+        # set cookie
+        res.set_cookie(COOKIE_KEY, token)
+        return res
+    else:
+        return jsonify({'result': 'fail', 'msg': '아이디와 비밀번호를 확인해 주세요 😓'})
+
+
+@app.route('/check_if_login', methods=["GET"])
+@login_required
+def check_if_login():
+    user = g.user['nickname']
+    userId = g.user['userId']
+
+    return jsonify({'result': 'success', 'user': user, 'userId':userId})
+
+
+@app.route('/logout', methods=["POST"])
+def logout():
+    res = make_response(jsonify({'result': 'success', 'msg': '👋로그아웃 완료👋'}))
+    # cookie 삭제
+    res.delete_cookie(COOKIE_KEY)
+
+    return res
 
 
 if __name__ == '__main__':
--- db_4plix.py
+++ db_4plix.py
@@ -10,7 +10,7 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
 
 
-def get_info():
+def get_netflix_list():
     for i in range(1, 9700):
         try:
 
@@ -45,12 +45,10 @@ def get_info():
     ])
 
 
-contents = list(db.fourflix.find())
-
-
 # 다음에 재검색해서 나머지 정보들 가져오기
 # 검색하기 쉽게 우선 데이터를 드라마, 영화, 쇼로 나눠 놓는
 def classify_type():
+    contents=[]
     """types = list(db.movies.aggregate([
         {"$group": {
             "_id": "$type",
@@ -75,133 +73,120 @@ def classify_type():
 # 다음영화사이트에 코드번호로 재검색
 # 나머지 정보들 가져오기
 def getmovieinfo():
-    movies = list(db.movies.find())
+    movie = []
+    cnt = 5326
     base_url = "https://search.daum.net/search?w=tot&DA=YZR&t__nil_searchbox=btn&sug=&sugo=&sq=&o=&q="
 
-    for movie in movies:
-        title = movie["title"]
-        data = requests.get(base_url + urllib.parse.quote(title), headers=headers)
-        soup = BeautifulSoup(data.text, 'html.parser')
-
-        try:
-            code = soup.select_one("#movieTitle > a")["href"].split("=")[1]
-            movie_url = "https://movie.daum.net/moviedb/main?movieId="
-            info = requests.get(movie_url + code, headers=headers)
-            ssoup = BeautifulSoup(info.text, 'html.parser')
-
-            try:
-                thumbnail = soup.select_one("#nmovie_img_0 > a > img")["src"]
-            except:
-                thumbnail = "None"
-                pass
-
-            try:
-                country = ssoup.select_one(
-                    "#mArticle > div.detail_movie.detail_main > div.movie_detail > div.movie_basic > div.main_detail > div.detail_summarize > div > dl:nth-child(2) > dd:nth-child(3)").text
-            except:
-                country = "None"
-                pass
-
-            try:
-                genre = ssoup.select_one(
-                    "#mArticle > div.detail_movie.detail_main > div.movie_detail > div.movie_basic > div.main_detail > div.detail_summarize > div > dl:nth-child(2) > dd.txt_main").text
-            except:
-                genre = "None"
-                pass
-
-            try:
-                runtime = ssoup.select_one(
-                    "#mArticle > div.detail_movie.detail_main > div.movie_detail > div.movie_basic > div.main_detail > div.detail_summarize > div > dl:nth-child(3) > dd:nth-last-child(1)").text.split(
-                    "분")[0].strip()
-            except:
-                runtime = "None"
-                pass
-
-            try:
-                director = ssoup.select_one(
-                    "#mArticle > div.detail_movie.detail_main > div.movie_detail > div.movie_basic > div.main_detail > div.detail_summarize > div > dl:nth-child(4) > dd:nth-child(2) > a").text
-            except:
-                director = "None"
-                pass
-
-            try:
-                cast = ssoup.select_one(
-                    "#mArticle > div.detail_movie.detail_main > div.movie_detail > div.movie_basic > div.main_detail > div.detail_summarize > div > dl:nth-child(4) > dd:nth-child(4)").text.split(
-                    "주연")[1].strip()
-                cast = ''.join(cast.split())
-            except:
-                cast = "None"
-                pass
-
-            try:
-                desc = ssoup.select_one(
-                    "#mArticle > div.detail_movie.detail_main > div.movie_detail > div.movie_basic > div.main_detail > div.desc_movie > p").text.strip()
-            except:
-                desc = "None"
-                pass
-
-        except:
-            country = "None"
-            genre = "None"
-            runtime = "None"
-            director = "None"
-            cast = "None"
-            desc = "None"
-            thumbnail = "None"
-
-        finally:
-            db.movies.update_one({"title": title}, {"$set": {"country": country}})
-            db.movies.update_one({"title": title}, {"$set": {"genre": genre}})
-            db.movies.update_one({"title": title}, {"$set": {"runtime": runtime}})
-            db.movies.update_one({"title": title}, {"$set": {"director": director}})
-            db.movies.update_one({"title": title}, {"$set": {"cast": cast}})
-            db.movies.update_one({"title": title}, {"$set": {"desc": desc}})
-            db.movies.update_one({"title": title}, {"$set": {"thumbnail": thumbnail}})
-
-
-"""        try:
+    for m in movie:
+        # for i in range(10):
+        if len(list(db.totContents.find({"title": {"$regex": m}}, {'_id': False}))) == 0:
+            title = m
             data = requests.get(base_url + urllib.parse.quote(title), headers=headers)
             soup = BeautifulSoup(data.text, 'html.parser')
-            info = soup.select_one(
-                "#movieEColl > div.coll_cont > div > div.info_movie > div.wrap_cont > dl:nth-child(2) > dd.cont").text
-            # movieEColl > div.coll_cont > div > div.info_movie > div.wrap_cont.type_longtit5 > dl:nth-child(2) > dd.cont
 
-            country = info.lstrip().split("|")[0].split(" ")[0]
-            genre = info.split("|")[1].lstrip().split("|")[0].split(" ")[0]
-            runtime = info[-6:-2].strip()
+            try:
+                url = soup.select_one("#movieTitle > a")["href"]
+                info = requests.get(url, headers=headers)
+                ssoup = BeautifulSoup(info.text, 'html.parser')
 
-            if not country.isalpha():
+                try:
+                    thumbnail = soup.select_one("#nmovie_img_0 > a > img")["src"]
+                except:
+                    thumbnail = "None"
+                    pass
+
+                try:
+                    country = (soup.select_one(
+                        "#movieEColl > div.coll_cont > div > div.info_movie > div.wrap_cont > dl:nth-child(2) > dd.cont").text.split(
+                        '|'))[0].lstrip().split(' ')[0].strip()
+                except:
+                    country = "None"
+                    pass
+
+                try:
+                    genre = (soup.select_one(
+                        "#movieEColl > div.coll_cont > div > div.info_movie > div.wrap_cont > dl:nth-child(2) > dd.cont").text.split(
+                        '|'))[1].lstrip().split(' ')[0].strip()
+                except:
+                    genre = "None"
+                    pass
+
+                try:
+                    runtime = (soup.select_one(
+                        "#movieEColl > div.coll_cont > div > div.info_movie > div.wrap_cont > dl:nth-child(2) > dd.cont").text.split(
+                        '|'))[-1].split('분')[0].strip()
+                except:
+                    runtime = "None"
+                    pass
+
+                try:
+                    director = soup.select_one(
+                        "#movieEColl > div.coll_cont > div > div.info_movie > div.wrap_cont > dl:nth-child(3) > dd > a").text
+                except:
+                    director = "None"
+                    pass
+
+                try:
+                    cast = soup.select_one(
+                        "#movieEColl > div.coll_cont > div > div.info_movie > div.wrap_cont > dl:nth-child(4) > dd").text.split(
+                        '더보기')[0].strip()
+                except:
+                    cast = "None"
+                    pass
+
+                try:
+                    desc = ssoup.select_one(
+                        "#mainContent > div > div.box_detailinfo > div.detail_basicinfo > div > div > div").text.strip()
+                except:
+                    desc = "None"
+                    pass
+
+                try:
+                    year = soup.select_one("#movieTitle > span").text.split(',')[1].split('제작')[0].strip()
+                except:
+                    year = "None"
+                    pass
+
+            except:
                 country = "None"
-            if not genre.isalpha():
                 genre = "None"
-            if runtime.isdigit():
-                runtime = int(runtime)
-            else:
                 runtime = "None"
+                director = "None"
+                cast = "None"
+                desc = "None"
+                thumbnail = "None"
+                year = "None"
 
-        except:
-            country = "None"
-            genre = "None"
-            runtime = "None"
+            finally:
+                doc = {
+                    'title': title,
+                    'type': '영화',
+                    'country': country,
+                    'genre': genre,
+                    'runtime': runtime,
+                    'director': director,
+                    'cast': cast,
+                    'desc': desc,
+                    'thumbnail': thumbnail,
+                    'contentId': cnt,
+                    'year': year
+                }
 
-        finally:
-            db.movies.update_one({"title": title}, {"$set": {"country": country}})
-            db.movies.update_one({"title": title}, {"$set": {"genre": genre}})
-            db.movies.update_one({"title": title}, {"$set": {"runtime": runtime}})"""
+            db.totContents.insert_one(doc)
+            print(doc)
+            cnt += 1
 
 
-# NB. Original query string below. It seems impossible to parse and
-# reproduce query strings 100% accurately so the one below is given
-# in case the reproduced version is not "correct".
-# response = requests.get('https://search.daum.net/qsearch?mk=YDDAQ8DNoAPnZ4Ux7k%40jmAAAAAk&uk=YDDAQ8DNoAPnZ4Ux7k%40jmAAAAAk&q=95073.json&viewtype=json&w=movie&m=comment', headers=headers)
+def getdramainfo():
+    drama = ['운석전', '뉴 암스테르담', '결혼작사 이혼작곡','손 the guest','루갈','구해줘','도시괴담','배가본드','싸우자 귀신아','빙의','나쁜 녀석들','우리 사랑했을까','우아한 가']
+    cnt = 5357
 
-
-def getinfo():
-    shows = list(db.shows.find())
     base_url = "https://search.daum.net/search?w=tot&DA=YZR&t__nil_searchbox=btn&sug=&sugo=&sq=&o=&q="
-    for show in shows:
+    for d in drama:
+        if len(list(db.totContents.find({"title": {"$regex": d}}, {'_id': False}))) != 0:
+            continue
         try:
-            title = show["title"]
+            title = d
             data = requests.get(base_url + urllib.parse.quote(title), headers=headers)
             soup = BeautifulSoup(data.text, 'html.parser')
 
@@ -213,17 +198,27 @@ def getinfo():
                 pass
 
             try:
-                category = soup.select_one(
+                country = soup.select_one(
                     "#tvpColl > div.coll_cont > div > div.head_cont > div > span:nth-child(2)").text
+                if country == '드라마':
+                    country = '한국드라마'
             except:
                 country = "None"
+                pass
+
+            try:
+                year = soup.select_one(
+                    "#tvpColl > div.coll_cont > div > div.head_cont > div > span:nth-child(4)").text.split('.')[0]
+
+            except:
+                year = "None"
                 pass
 
             try:
                 cast_list = soup.select("#tv_program > div.wrap_col.castingList.lst > ul li")
                 temp = []
                 for c in cast_list:
-                    name = c.select_one("span.txt_name > a").text.strip()
+                    name = c.select_one("span.sub_name > a").text.strip()
                     temp.append(name)
 
                 if len(temp) != 0:
@@ -241,131 +236,35 @@ def getinfo():
                 pass
 
         except:
-            category = "None"
+            country = "None"
             thumbnail = "None"
             cast = "None"
             desc = "None"
+            year = 'None'
 
         finally:
-            db.shows.update_one({"title": title}, {"$set": {"category": category}})
-            db.shows.update_one({"title": title}, {"$set": {"thumbnail": thumbnail}})
-            db.shows.update_one({"title": title}, {"$set": {"cast": cast}})
-            db.shows.update_one({"title": title}, {"$set": {"desc": desc}})
+            doc={
+                'title': title,
+                'type': '드라마',
+                'country':country,
+                'thumbnail':thumbnail,
+                'cast':cast,
+                'desc':desc,
+                'year':year,
+                'contentId':cnt
+            }
+
+        db.totContents.insert_one(doc)
+        print(doc)
+        cnt+=1
+
+
 
 
 # db.collection.update_many({},{"$rename":{"oldName":"newName"}}) renmae field
 # db.collection.update_many({},{"$unset":{"filedName":1}}) delete field
 
-drama = ['도시남녀의 사랑법', '경우의 수', '상견니', '첫사랑의 멜로디', '열여덟의 순간', '김비서가 왜그럴까', '백일의 낭군님', '디어 마이 프렌즈', '애타는 로맨스', '안녕? 나야!',
-         'the k2', '보이스 1', '모범형사', '아는 와이프', '터널', '리갈 하이', '우아한 친구들']
-movie = ['트와일라잇', '죽지않는 인간들의 밤', '적선인 흑마살수', '365일', '킹 아서: 제왕의 검', '날씨의 아이', '목소리의 형태', '조작된 도시', '너의 결혼식', '컨저링',
-         '탐정 더 비기닝', '문신을 한 신부님', '뉴문', '마션', '아가씨', '인헤리턴스', '시간 이탈자', '모노노케 히메', '신비한 동물사전', '신과 함께 인과 연', '레베카',
-         '브레이킹 던', '나의 소녀시대', '너의 이름은', '나는 내일, 어제의 너와 만난다', '키싱부스', '건축학 개론', '너와 100번째 사랑', '말 할 수 없는 비밀', '피끓는 청춘',
-         '눈물이 주룩주룩',
-         '울고싶은 나는 고양이 가면을 쓴다', '8월의 크리스마스', '조제, 호랑이 그리고 물고기들', '지금, 만나러 갑니다', '이클립스', '어거스트 러쉬', '악마는 프라다를 입는다',
-         '엣지 오브 투모로', '컨테이젼',
-         '가타카', '올드 가드', '캐스트 어웨이', '미스 페레그린과 이상한 아이들의 집', '로맨틱 홀리데이', '패밀리 맨', '에린 브로코비치', '월드워Z', '데드풀', '메멘토',
-         '완벽한 타인',
-         '극한직업', '곤지암', '변신', '협상']
 
-# cnt = 5271
-#
-# base_url = "https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=1&ie=utf8&query="
-#
-# for m in movie:
-#     title = m
-#     data = requests.get(base_url + urllib.parse.quote(title), headers=headers)
-#     soup = BeautifulSoup(data.text, 'html.parser')
-#
-#     try:
-#         code = soup.select_one("#main_pack > div.sc_new.cs_common_module.case_empasis.color_15._au_movie_content_wrap > div.cm_top_wrap._sticky > div.title_area._title_area > h2 > a")["href"].split("=")[1]
-#         movie_url = "https://movie.naver.com/movie/bi/mi/basic.nhn?code="
-#         info = requests.get(movie_url + code, headers=headers)
-#         ssoup = BeautifulSoup(info.text, 'html.parser')
-#
-#         try:
-#             thumbnail = soup.select_one("#main_pack > div.sc_new.cs_common_module.case_empasis.color_4._au_movie_content_wrap > div.cm_content_wrap > div.cm_content_area._cm_content_area_info > div.cm_info_box > div.detail_info > a > img")["src"]
-#         except:
-#             thumbnail = "None"
-#             pass
-#
-#         try:
-#             country = soup.select_one("#main_pack > div.sc_new.cs_common_module.case_empasis.color_4._au_movie_content_wrap > div.cm_content_wrap > div.cm_content_area._cm_content_area_info > div.cm_info_box > div.detail_info > dl > div:nth-child(1) > dd").text()
-#         except:
-#             country = "None"
-#             pass
-#
-#         try:
-#             genre = soup.select_one(
-#                 "#main_pack > div.sc_new.cs_common_module.case_empasis.color_4._au_movie_content_wrap > div.cm_content_wrap > div.cm_content_area._cm_content_area_info > div.cm_info_box > div.detail_info > dl > div:nth-child(1) > dd").text
-#
-#         except:
-#             genre = "None"
-#             pass
-#
-#         try:
-#             runtime = soup.select_one(
-#                 "#main_pack > div.sc_new.cs_common_module.case_empasis.color_4._au_movie_content_wrap > div.cm_content_wrap > div.cm_content_area._cm_content_area_info > div.cm_info_box > div.detail_info > dl > div:nth-child(1) > dd").text
-#         except:
-#             runtime = "None"
-#             pass
-#
-#         try:
-#             director = soup.select_one(
-#                 "#content > div.article > div.mv_info_area > div.mv_info > dl > dd:nth-child(4) > p > a").text
-#         except:
-#             director = "None"
-#             pass
-#
-#         try:
-#             cast_list = soup.select_one("#content > div.article > div.mv_info_area > div.mv_info > dl > dd:nth-child(6) > p a")
-#             temp=[]
-#             for c in cast_list:
-#                 temp.append(c.select_one("div > div > strong > a").text)
-#             cast = ', '.join(temp[1:])
-#
-#
-#         except:
-#             cast = "None"
-#             pass
-#
-#         try:
-#             desc = ssoup.select_one(
-#                 "#content > div.article > div.section_group.section_group_frst > div:nth-child(1) > div > div.story_area > p").text.strip()
-#         except:
-#             desc = "None"
-#             pass
-#
-#         try:
-#             year = soup.select_one("#main_pack > div.sc_new.cs_common_module.case_empasis.color_15._au_movie_content_wrap > div.cm_top_wrap._sticky > div.title_area._title_area > div > span:nth-child(5)").text
-#         except:
-#             year = "None"
-#             pass
-#
-#     except:
-#         country = "None"
-#         genre = "None"
-#         runtime = "None"
-#         director = "None"
-#         cast = "None"
-#         desc = "None"
-#         thumbnail = "None"
-#         year = "None"
-#
-#     finally:
-#         doc = {
-#             'title': title,
-#             'type': '영화',
-#             'country': country,
-#             'genre': genre,
-#             'runtime': runtime,
-#             'director': director,
-#             'cast': cast,
-#             'desc': desc,
-#             'thumbnail': thumbnail,
-#             'contentId': cnt,
-#             'year':year
-#         }
-#
-#     print(doc)
-#     cnt += 1
+
+
+
